08a86e0cc8d7c201b47afc25d4c13a69a0cf9108
Jelle Licht <wordempire@gmail.com>
2015-02-12 17:04:36 +0100

    First version of Clock

--- DistributedAlgorithmsLab/src/impl/Clock.java
+++ DistributedAlgorithmsLab/src/impl/Clock.java
@@ -1,5 +1,30 @@
 package impl;
 
-public class Clock {
-
+public class Clock implements Comparable<Clock>{
+	private int tick;
+	private int pid;
+	
+	public Clock(int pid){
+		this.tick = 0;
+		this.pid = pid;
+	}
+	
+	public void inc(){
+		this.tick++;
+	}
+	
+	public void sync(Clock o){
+		this.tick = Math.max(this.tick, o.tick);
+		this.inc();
+	}
+	
+	@Override
+	public int compareTo(Clock o) {
+		// TODO Auto-generated method stub
+		
+		if(this.tick < o.tick) { return -1; }
+		else if (this.tick == o.tick && this.pid < o.pid) { return -1; }
+		else { return 1; }
+	}
+	
 }
