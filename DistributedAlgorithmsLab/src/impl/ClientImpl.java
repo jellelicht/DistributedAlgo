9a513b4da72648d232d33c591f311c1b9b95619c
Jelle Licht <wordempire@gmail.com>
2015-02-12 16:25:41 +0100

    Made sure the ClientImpl implements the correct message stubs for the Client interface

--- DistributedAlgorithmsLab/src/impl/ClientImpl.java
+++ DistributedAlgorithmsLab/src/impl/ClientImpl.java
@@ -1,11 +1,41 @@
 package impl;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-public class ClientImpl {
+import listeners.AckListener;
+import listeners.MessageListener;
+import model.Client;
+import model.Message;
+
+public class ClientImpl implements Client {
 
 	public void activate(ArrayList<ClientImpl> clients) {
 		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void msg(Message m) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ack(Message m) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addMessageListener(MessageListener m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addAckListener(AckListener a) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
