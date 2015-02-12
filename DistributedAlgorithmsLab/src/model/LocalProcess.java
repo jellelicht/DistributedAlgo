740fa622331dd2078280e92852bb5837e0dcce1e
Jelle Licht <wordempire@gmail.com>
2015-02-12 17:07:09 +0100

    And now with nice Message Handling

--- DistributedAlgorithmsLab/src/model/LocalProcess.java
+++ DistributedAlgorithmsLab/src/model/LocalProcess.java
@@ -6,7 +6,7 @@ import listeners.MessageListener;
 
 public interface LocalProcess {
 	//public void addPeersChangedListener(PeersChangedListener p);
-	
+	public void deliverMessage(Message m);
 	//public void addMessageListener(MessageListener m);
 	//public void addAckListener(AckListener a);	
 }
