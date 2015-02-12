b9e2d1f5ac0948f2c390c27157789dd28fccfbe4
Brynjólfur Már Georgsson <binnimar3319@gmail.com>
2015-02-12 14:09:42 +0100

    Project creations

---
+++ DistributedAlgorithmsLab/src/MyRMI.java
@@ -0,0 +1,8 @@
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
+
+public interface MyRMI extends Remote {
+	
+	public void testMethod(int arg) throws java.rmi.RemoteException;
+
+}
---
+++ DistributedAlgorithmsLab/src/MyRMIImp.java
@@ -0,0 +1,16 @@
+import java.rmi.*;
+
+
+public class MyRMIImp implements MyRMI {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void testMethod(int arg) throws RemoteException {
+		// Create and install a security manager
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new RMISecurityManager());
+		}
+		
+	}
+
+}
